f3862752641e100890bec5363f94140461217a8e
yossiffarouk <yossif155farouk@gmail.com>
2025-04-15 11:26:31 +0200

    ConnectionString And Category Added

---
+++ BookSIte/Data/Context.cs
@@ -0,0 +1,16 @@
+﻿using BookSIte.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookSIte.Data
+{
+    public class Context : DbContext
+    {
+
+        public Context(DbContextOptions<Context> options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Category> Category { get; set; }
+    }
+}
--- BookSIte/Program.cs
+++ BookSIte/Program.cs
@@ -1,7 +1,14 @@
+using BookSIte.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<Context>
+    (options => options.UseSqlServer
+    (builder.Configuration.GetConnectionString("DefualtConnection")));
 
 var app = builder.Build();
 
