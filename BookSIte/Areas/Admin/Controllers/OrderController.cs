b33aa0b9ed0635e8d5fa02c6e28dabdfad5fbb99
yossiffarouk <yossif155farouk@gmail.com>
2025-04-27 10:16:15 +0300

    order mangment for admin added

---
+++ BookSIte/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,32 @@
+﻿using BookSite.DataAccess.Repository.Unitofwork;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BookSIte.Areas.Admin.Controllers
+{
+	[Area("Admin")]
+	public class OrderController : Controller
+	{
+		private readonly IUnitOfWork _Unit;
+		
+
+		public OrderController(IUnitOfWork Unit)
+		{
+			_Unit = Unit;
+		
+		}
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+
+
+
+		public IActionResult getall()
+		{
+			var Orders = _Unit.OrderHeaderRepo.GetAll(includeproperty: "ApplicationUser");
+			
+			return Json(new { data = Orders });
+		}
+	}
+}
---
+++ BookSIte/wwwroot/js/order.js
@@ -0,0 +1,32 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": { url: '/admin/order/getall' },
+        "columns": [
+            { "data": "id", "width": "10%" },
+            { "data": "name", "width": "10%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "applicationUser.email", "width": "15%" },
+            { "data": "orderStatus", "width": "15%" },
+            { "data": "orderTotal", "width": "15%" },
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group" role="group">
+                     <a href="/admin/order/details?orderId=${data}" class="btn btn-primary mx-2"> <i class="bi bi-pencil-square"></i></a>    
+                    
+                    </div>`
+                },
+                "width": "20%"
+            }
+        ]
+    });
+}
+
+
