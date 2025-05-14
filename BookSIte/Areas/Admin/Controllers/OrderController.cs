1f0a9ff773812047a54c60c5dc95db265f92985b
yossiffarouk <yossif155farouk@gmail.com>
2025-05-14 18:57:02 +0300

    Make stripe domin more dynmic for deployment

--- BookSIte/Areas/Admin/Controllers/OrderController.cs
+++ BookSIte/Areas/Admin/Controllers/OrderController.cs
@@ -140,7 +140,7 @@ namespace BookSIte.Areas.Admin.Controllers
 
             //it is a regular customer account and we need to capture payment
             //stripe logic
-            var domain = "https://localhost:7198/";
+            var domain = Request.Scheme + "//" + Request.Host.Value + "/" ;
             var options = new SessionCreateOptions
             {
                 SuccessUrl = domain + $"Admin/Order/PaymentConfirmation?orderHeaderId={orderVM.Header.Id}",
